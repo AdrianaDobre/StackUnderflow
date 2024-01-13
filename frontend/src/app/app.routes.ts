b5b173be2af6cc922d897dc7c20f33a57a867189
TeodoraLazaroiu <lazaroiuteodora@yahoo.com>
2024-01-13 15:50:04 +0200

    added not found page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { PostComponent } from './post/post.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginFormComponent, pathMatch: 'full'},
     {path: 'register', component: RegisterFormComponent, pathMatch: 'full'},
     {path: 'view/:id', component: PostComponent},
+    {path: '', component: PostComponent},
+    {path: '**', component: NotFoundComponent}
 ];
